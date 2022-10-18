131d59d36cc9951808ff84e2471c7406af79042c
Edyta Dudek <edyta.dudek@10g.pl>
2022-10-18 15:10:53 +0200

    menu hamburger added

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,38 @@ closeLink.addEventListener('click', (e) => {
     e.preventDefault();
 
     infCookie.setCookie();
-})
+});
+
+
+const hamburger = document.querySelector('#menu');
+
+const nav = document.querySelector('nav');
+
+const link = document.querySelectorAll('.nav__item');
+
+const body = document.querySelector('body');
+
+
+function closeAll(){
+    
+    nav.classList.remove('display-block');
+    
+    hamburger.classList.remove('display-none');
+}
+
+hamburger.addEventListener('click', (e)=>{
+    
+    e.stopPropagation(); 
+   
+    
+    nav.classList.add('display-block');
+
+    hamburger.classList.add('display-none');
+
+});
+
+body.addEventListener('click', closeAll);
+
 
 
 
